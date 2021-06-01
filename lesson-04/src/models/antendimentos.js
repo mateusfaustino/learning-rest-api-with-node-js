6153c85be08557d6f88de63b0bf66463bc3d6bd3
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-06-01 14:22:39 -0300

    error handling

--- lesson-04/src/models/antendimentos.js
+++ lesson-04/src/models/antendimentos.js
@@ -2,9 +2,31 @@ const moment = require('moment');
 const conexao =require('../infraestrutura/conection');
 class Atendimento {
     add(atendimento, res){
-        const dataCriacao = new Date();
-        console.log("dataCriacao: ",dataCriacao);
+        const dataCriacao = new Date(); 
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const isDateValid = moment(data).isSameOrAfter(dataCriacao);
+        const isCostumerNameValid = atendimento.cliente.length >= 4;
+
+        
+        const validations = [
+            {
+                name:'data',
+                isValid: isDateValid,
+                message:'A data deve ser maior ou igual a data atual'
+            },
+            {
+                name:'cliente',
+                isValid: isCostumerNameValid,
+                message: 'O nome do cliente deve ter pelo menos 4 caracteres'
+            }
+        ]
+        
+        const errors = validations.filter(campo => !campo.isValid )
+        const isThereErrors = errors.length;
+
+        if (isThereErrors) {
+            res.status(400).json(errors)
+        } else {
         const atendimentoDatado = {...atendimento, dataCriacao, data}
         const sql = 'INSERT INTO Atendimentos SET ?'
         conexao.query(sql, atendimentoDatado, (error, results)=>{
@@ -13,7 +35,9 @@ class Atendimento {
             } else {
                 res.status(201).json(results);
             }
-        })
+        })            
+        }
+
     }
 }
 
